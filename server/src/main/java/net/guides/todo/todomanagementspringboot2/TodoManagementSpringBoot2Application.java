55c9fcca5edc03a73c7b0e4532bac25f49c8cf31
Christian Santos <sancheschris07@gmail.com>
2020-03-05 14:46:30 -0300

    first commit

---
+++ server/src/main/java/net/guides/todo/todomanagementspringboot2/TodoManagementSpringBoot2Application.java
@@ -0,0 +1,13 @@
+package net.guides.todo.todomanagementspringboot2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TodoManagementSpringBoot2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TodoManagementSpringBoot2Application.class, args);
+	}
+
+}
